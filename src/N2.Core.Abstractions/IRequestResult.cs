b16a0fa78a2ba1ad6fa97311fb8c7b4551761a72
gjhkaal@gmail.com <gerritjankaal@cadac.com>
2025-04-24 12:04:51 +0200

    Add handler interface

--- src/N2.Core.Abstractions/IRequestResult.cs
+++ src/N2.Core.Abstractions/IRequestResult.cs
@@ -1,8 +1,36 @@
-﻿namespace N2.Core;
+namespace N2.Core;
 
 public interface IRequestResult
 {
     bool IsSuccessCode { get; }
     string Message { get; }
     int ResultCode { get; }
+}
+
+
+/// <summary>
+/// Response model for a handler.
+/// </summary>
+public interface IResponse
+{
+}
+
+/// <summary>
+/// Request model for a handler.
+/// </summary>
+public interface IRequest
+{
+
+}
+
+/// <summary>
+/// Handler interface.
+/// </summary>
+/// <typeparam name="TRequest">A request model.</typeparam>
+/// <typeparam name="TResponse">A response model.</typeparam>
+public interface IHandle<TRequest, TResponse>
+    where TRequest : IRequest
+    where TResponse : IResponse
+{
+    Task<TResponse> Handle(TRequest request, CancellationToken token);
 }
