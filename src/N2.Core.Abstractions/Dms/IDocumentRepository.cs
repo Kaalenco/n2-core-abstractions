4dd2ee6e10a70497eee7f38ececa75586aa790e3
gjhkaal@gmail.com <gjhkaal@gmail.com>
2025-10-13 10:58:10 +0200

    Update IDocumentRepository

--- src/N2.Core.Abstractions/Dms/IDocumentRepository.cs
+++ src/N2.Core.Abstractions/Dms/IDocumentRepository.cs
@@ -7,19 +7,23 @@ namespace N2.Core.Dms;
 /// </summary>
 public interface IDocumentRepository
 {
-    Task<int> CompleteAsync(IUserContext userMatrix);
+    Task<int> CompleteAsync(IUserContext userMatrix, CancellationToken token);
 
     bool DocumentExists(string fileIdentifier);
 
     void SaveDocument(Document file);
 
-    Task<Document> FindDocumentAsync(Guid publicId);
+    Task<Document> FindDocumentAsync(Guid publicId, CancellationToken token);
 
-    Task<Document> FindDocumentAsync(Guid publicId, bool isDeleted);
+    Task<Document> FindDocumentAsync(Guid publicId, bool isDeleted, CancellationToken token);
 
-    Task<Document> FindDocumentAsync(Guid publicId, bool isDeleted, bool isPublic, bool isEnabled);
+    Task<Document> FindDocumentAsync(Guid publicId, bool isDeleted, bool isPublic, bool isEnabled, CancellationToken token);
 
-    Task<Document> FindDocumentAsync(Guid publicId, bool isDeleted, Guid createdBy);
+    Task<Document> FindDocumentAsync(Guid publicId, bool isDeleted, Guid createdBy, CancellationToken token);
+
+    Task RemoveDocument(Guid publicId, CancellationToken token);
+
+    Task Cleanup(CancellationToken token);
 
     IQueryable<Document> DocumentQuery { get; }
 }
